982d06e4363184ada8901f60dc007064adc5eea3
TranAnhDat1 <tranadat1428@gmail.com>
2025-02-18 16:34:35 +0700

    Add files via upload

---
+++ bai4.php
@@ -0,0 +1,7 @@
+<?php
+$students = [
+    ['hoTen' => 'Nguyen Van A', 'ngaySinh' => '2000-01-01', 'gioiTinh' => 'Nam', 'diemToan' => 8, 'diemVan' => 7, 'diemTiengAnh' => 9],
+    ['hoTen' => 'Tran Thi B', 'ngaySinh' => '2001-02-02', 'gioiTinh' => 'Nu', 'diemToan' => 9, 'diemVan' => 8, 'diemTiengAnh' => 8]
+    // Thêm 18 sinh viên nữa
+];
+?>
